8577652d56d50edf7cc6d407858ea896b6d69940
Andre Pacheco <andregoncalvespacheco@hotmail.com>
2025-02-14 13:26:26 -0300

    Calculator (LCD + Serial Monitor)

---
+++ Arduino-IDE-Projects/SerialMonitorCalculator/SerialMonitorCalculator.ino
@@ -0,0 +1,58 @@
+#include <LiquidCrystal.h>
+int rs = 7;
+int en = 8;
+int d4 = 9;
+int d5 = 10;
+int d6 = 11;
+int d7 = 12;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+}
+
+void waitInput(){
+  while (!Serial.available()) {
+  
+  }
+}
+
+void loop() {
+  Serial.println("Type the first number: ");
+  waitInput();
+  int num1 = Serial.parseInt();
+
+  Serial.println("Type the operation:");
+  waitInput();
+  String operation = Serial.readString();
+
+  Serial.println("Type the second number: ");
+  waitInput();
+  int num2 = Serial.parseInt();
+  
+  int result = 0;
+
+  if(operation == "+")
+    result = num1 + num2;
+  if(operation == "-")
+    result = num1 - num2;
+  if(operation == "*")
+    result = num1 * num2;
+  if(operation == "/")
+    result = num1 / num2;
+
+  lcd.setCursor(0, 0);
+  lcd.print("Result:");
+  lcd.setCursor(0, 1);
+  lcd.print(num1);
+  lcd.print(operation);
+  lcd.print((num2));
+  lcd.print(" = ");
+  lcd.print(result);
+
+
+}
