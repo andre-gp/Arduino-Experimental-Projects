46befe4b9ac6de9ab32e41d4498d4fc500e6bc66
Andre Pacheco <andregoncalvespacheco@hotmail.com>
2025-02-23 20:35:54 -0300

    Add RTC Clock to Weather Log

--- PlatformIO-Projects/WeatherLog/src/main.cpp
+++ PlatformIO-Projects/WeatherLog/src/main.cpp
@@ -2,10 +2,9 @@
 #include <SD.h>
 #include <SPI.h>
 #include <DHT.h>
+#include <RTClib.h>
 
 #define FILE_NAME "log.txt"
-
-File fileLog;
 
 /* SD Card Reader Pins */
 int CS = 10;
@@ -16,11 +15,27 @@ int ledPin =  7;
 /* Sensor Pin*/
 int sensorPin = 2;
 
+File fileLog;
+
 DHT sensor(sensorPin, DHT11);
 
+RTC_DS1307 rtc;
 
 void setup() {
 	Serial.begin(9600);
+
+	while(!rtc.begin())
+	{
+	    Serial.println("RTC was not found.");
+	}
+
+	if(!rtc.isrunning())
+	{
+	    Serial.println("ERROR: RTC is not running!");
+		Serial.println("Adjusting to time of compilation.");
+		rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+	}
+
 	pinMode(CS, OUTPUT);
 	pinMode(ledPin, OUTPUT);
 	
@@ -43,7 +58,7 @@ void setup() {
 	{
 		Serial.println("Opened the file successfully!");
 		
-		fileLog.println(" = = = NEW SESSION = = =");
+		fileLog.println(" = = = NEW SESSION (André Pacheco) = = =");
 		
 		fileLog.close();
 	}
@@ -55,27 +70,46 @@ void setup() {
 	digitalWrite(ledPin, LOW);
 }
 
+void printNum(uint16_t digit)
+{
+    if(digit < 10)
+		fileLog.print(0);
+
+	fileLog.print(digit);
+}
+
 void printTime()
 {
-	unsigned long currentMillis = millis();
-	unsigned long seconds = currentMillis / 1000;
-	unsigned long minutes = seconds / 60;
-	unsigned long hours = minutes / 60;
-	unsigned long days = hours / 24;
-	
-	fileLog.print(hours);
-	fileLog.print("::");
-	fileLog.print(minutes);
-	fileLog.print("::");
-	fileLog.print(seconds);
-	fileLog.print("::");
-	fileLog.print(currentMillis);
+	// unsigned long currentMillis = millis();
+	// unsigned long seconds = currentMillis / 1000;
+	// unsigned long minutes = seconds / 60;
+	// unsigned long hours = minutes / 60;
+	// unsigned long days = hours / 24;
+
+	// currentMillis %= 1000;
+	// seconds %= 60;
+	// minutes %= 60;
+	// hours %= 24;
+
+	DateTime time = rtc.now();
+
+	printNum(time.day());
+	fileLog.print("/");
+	printNum(time.month());
+	fileLog.print("/");
+	printNum(time.year());
+
+	fileLog.print(" | ");
+
+	printNum(time.hour());
+	fileLog.print(":");
+	printNum(time.minute());
+	fileLog.print(":");
+	printNum(time.second());
 }
 
 void loop() {
 	delay(5000);
-	
-	digitalWrite(ledPin, HIGH);
 	
 	float humidity = sensor.readHumidity();
 	float temp = sensor.readTemperature();
@@ -84,16 +118,16 @@ void loop() {
 	
 	if(fileLog)
 	{
+		digitalWrite(ledPin, HIGH);
+
 		Serial.println("Opened the file successfully!");
 		
 		printTime();
 		
 		fileLog.print(" | ");
-		fileLog.print(" Temperature: ");
 		fileLog.print(temp);
 		
 		fileLog.print(" | ");
-		fileLog.print(" Humidity: ");
 		fileLog.println(humidity);
 		
 		fileLog.close();
